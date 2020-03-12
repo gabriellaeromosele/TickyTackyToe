c166bbc56676da3ac29acae5c4a0f79c818d9c0a
Student_M23 <Student_M23@ps-dal-m23.home>
2020-03-11 22:42:59 -0500

    Adding existing file

---
+++ Pseudocode/script.js
@@ -0,0 +1,36 @@
+TICKY TACKY TOE
+
+Alert
+Have an alert that promts the user that after 3 rounds the game will be over 
+ 
+<OR> 
+
+I will have a Pop up thats states game will be over after 3 rounds - Modal
+
+OBJECTS
+Player 1 {
+    color: Pink;
+    letter: X;
+}
+
+Player 2 {
+    color: Purple;
+    letter: O;
+}
+
+Functions
+Create a funtion that decides who will go first
+Create a function that Starts the Game
+Create a function that Resets the Game 
+
+
+Loops 
+Create a loop that states "Player 1 or Player 2 turn"
+Counting the score using loops
+Once a player goes that box is locked and can no longer 
+Create a loop that will state the GAME IS OVER after the third round
+
+While/if loops
+Use while / if statements that console.log You Won! or You Lost else It's a Draw!
+
+Do loops - research needed
